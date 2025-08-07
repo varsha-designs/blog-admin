3bbf4795fe0a666c64cfa256b4fd15157e5af2f8
varsha <varshabharathi.642@gmail.com>
2025-08-07 07:01:12 +0530

    Added fields to posts table and ran migration

--- database/migrations/2025_08_07_012749_create_posts_table.php
+++ database/migrations/2025_08_07_012749_create_posts_table.php
@@ -12,8 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('posts', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+           $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('content');
+            $table->enum('status', ['draft', 'published'])->default('draft');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+
         });
     }
 
