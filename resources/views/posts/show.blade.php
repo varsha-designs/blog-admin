1cf5f768bfca270d56bcd1838e3dc1219a60536c
varsha <varshabharathi.642@gmail.com>
2025-08-11 13:16:30 +0530

    Added the lines for category view part

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,7 +7,8 @@
     <div class="bg-white p-6 rounded shadow">
         <p class="text-gray-700 mb-4"><strong>Status:</strong> {{ ucfirst($post->status) }}</p>
         <p class="text-gray-700 mb-4"><strong>Content:</strong></p>
-        <p class="text-gray-800 whitespace-pre-line">{{ $post->content }}</p>
+        <p class="text-gray-800 whitespace-pre-line">{{ $post->content }}</p><br>
+        <p class="text-gray-700 mb-4"><strong>Category:</strong> {{ ucfirst($post->category) }}</p>
 
         <div class="mt-6">
             <a href="{{ route('posts.edit', $post) }}" class="bg-yellow-500 text-white px-4 py-2 rounded hover:bg-yellow-600">Edit</a>
