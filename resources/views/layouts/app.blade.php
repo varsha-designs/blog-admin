e0546ebbf9efc51134f27249ee89bb8c536c851a
varsha <varshabharathi.642@gmail.com>
2025-08-22 16:27:48 +0530

    Removed the unnecessary word from app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="csrf-token" content="{{ csrf_token() }}" />
-    <title>@yield('title', 'Blog Post CRUD')</title>
+    <title>@yield('title')</title>
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
