8dd6da7469175c09041af59d234167dc91f4f888
varsha <varshabharathi.642@gmail.com>
2025-08-23 19:57:42 +0530

    Removed the category from the page

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -13,7 +13,6 @@
             <tr class="bg-gray-100 text-left">
                 <th class="py-2 px-4">Title</th>
                 <th class="py-2 px-4">Status</th>
-                <th class="py-2 px-4">Category</th>
                 <th class="py-2 px-4">Actions</th>
             </tr>
         </thead>
@@ -22,7 +21,6 @@
                 <tr class="border-t">
                     <td class="py-2 px-4">{{ $post->title }}</td>
                     <td class="py-2 px-4">{{ ucfirst($post->status) }}</td>
-                     <td class="py-2 px-4">{{ ucfirst($post->category->name) }}</td>
                     <td class="py-2 px-4 space-x-2">
                         <a href="{{ route('posts.show', $post) }}" class="text-blue-500 hover:underline">View</a>
                         <a href="{{ route('posts.edit', $post) }}" class="text-yellow-500 hover:underline">Edit</a>
