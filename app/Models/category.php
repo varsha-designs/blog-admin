53c2943ace32fce612f0ecca2f51c1b53a10aece
varsha <varshabharathi.642@gmail.com>
2025-08-25 16:49:06 +0530

    Added the fillable

--- app/Models/category.php
+++ app/Models/category.php
@@ -7,11 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     // only if table name is 'category', else Laravel assumes 'categories'
-    protected $fillable = ['name', 'slug'];
+    protected $fillable = ['id', 'title', 'slug', 'content', 'category', 'user_id','categories_id'];
 
     public function posts()
     {
         return $this->hasMany(Post::class);
     }
+
 }
 
