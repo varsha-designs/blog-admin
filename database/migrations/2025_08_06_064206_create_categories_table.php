107e2e0c27e9f6e56a1993aaace873e9a1fa35b7
varsha <varshabharathi.642@gmail.com>
2025-08-25 16:49:58 +0530

    Added the field for database columns

--- database/migrations/2025_08_06_064206_create_categories_table.php
+++ database/migrations/2025_08_06_064206_create_categories_table.php
@@ -13,9 +13,11 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-             $table->string('name');
-            $table->string('slug')->unique();
-            $table->timestamps();
+           $table->string('title');
+            $table->string('slug');
+            $table->text('content');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+             $table->timestamps();
         });
     }
 
