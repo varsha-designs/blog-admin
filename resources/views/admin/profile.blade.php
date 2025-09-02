3df0d13cf0695e573e43136035297af8d46ac7c1
varsha <varshabharathi.642@gmail.com>
2025-09-02 11:03:10 +0530

    Added the profile page for admin

---
+++ resources/views/admin/profile.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Profile')
+
+@section('content')
+    <h1 class="text-2xl font-bold mb-4">Profile</h1>
+    <p>This is your profile page.</p>
+@endsection
